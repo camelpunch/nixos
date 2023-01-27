f696872636b017b0c39b3687525ac051ab0fa822
Andrew Bruce <me@andrewbruce.net>
2023-01-27 10:21:42 +0000

    Import home manager before andrew.nix

--- configuration.nix
+++ configuration.nix
@@ -9,8 +9,8 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./focusrite-scarlett-18i8.nix
-      ./andrew.nix
       <home-manager/nixos>
+      ./andrew.nix
     ];
 
   # Bootloader.
