25c088cf6253c76a0f9330f67f0b612412d753af
Andrew Bruce <me@andrewbruce.net>
2023-01-28 23:45:40 +0000

    Terminator config

--- andrew.nix
+++ andrew.nix
@@ -50,5 +50,21 @@ export PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
         st = "status";
       };
     };
+    programs.terminator = {
+      enable = true;
+      config = {
+        profiles.default = {
+          background_color = "#282828";
+          cursor_color = "#aaaaaa";
+          font = "Source Code Pro Semibold 16";
+          foreground_color = "#ebdbb2";
+          palette = "#282828:#cc241d:#98971a:#d79921:#458588:#b16286:#689d6a:#a89984:#928374:#fb4934:#b8bb26:#fabd2f:#83a598:#d3869b:#8ec07c:#ebdbb2";
+          scrollback_infinite = true;
+          scrollbar_position = "disabled";
+          show_titlebar = false;
+          use_system_font = false;
+        };
+      };
+    };
   };
 }
