f930e1caa2d97984b8252dc4635bee45cfdeb284
Andrew Bruce <me@andrewbruce.net>
2023-01-28 23:33:02 +0000

    Remove newline from start of PS1

--- andrew.nix
+++ andrew.nix
@@ -32,6 +32,12 @@
       pkgs.wl-clipboard
       pkgs.yubikey-manager
     ];
+    programs.bash = {
+      enable = true;
+      bashrcExtra = ''
+export PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
+      '';
+    };
     programs.git = {
       enable = true;
       userName = "Andrew Bruce";
