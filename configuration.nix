025047e731cb7302a40e412d353e26c74e07b61b
Andrew Bruce <me@andrewbruce.net>
2023-01-28 22:36:44 +0000

    Add some CLI utils

--- configuration.nix
+++ configuration.nix
@@ -109,11 +109,14 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    bintools
+    du-dust
     gcc
     git
     gnumake
     gnupg
-    bintools
+    htop
+    iftop
     sysfsutils
     wget
   ];
