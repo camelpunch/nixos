a31b0bb2ca939a84b0bd323cf381fdc6dcc93ba9
Andrew Bruce <me@andrewbruce.net>
2023-02-08 10:55:49 +0000

    Open Kubernetes API port on unhinged

--- server/configuration.nix
+++ server/configuration.nix
@@ -93,7 +93,7 @@
   };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 6443 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
