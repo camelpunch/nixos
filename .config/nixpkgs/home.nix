9ccac9bc5e2466a6ae140e2c710902fe00d0b160
Andrew Bruce <me@andrewbruce.net>
2023-02-12 10:01:16 +0000

    Smaller terminator font

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -175,7 +175,7 @@
       profiles.default = {
         background_color = "#282828";
         cursor_color = "#aaaaaa";
-        font = "Source Code Pro Semibold 16";
+        font = "Source Code Pro Semibold 14";
         foreground_color = "#ebdbb2";
         palette = "#282828:#cc241d:#98971a:#d79921:#458588:#b16286:#689d6a:#a89984:#928374:#fb4934:#b8bb26:#fabd2f:#83a598:#d3869b:#8ec07c:#ebdbb2";
         scrollback_infinite = true;
