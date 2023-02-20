37f5a0ef3847b7aae3976da8057f821b818f5ea1
Andrew Bruce <me@andrewbruce.net>
2023-02-20 22:50:50 +0000

    Turn off monitor after 5s on unhinged

--- server/configuration.nix
+++ server/configuration.nix
@@ -40,6 +40,8 @@
     };
   };
 
+  boot.kernelParams = [ "consoleblank=5" ];
+
   networking.hostName = ""; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
