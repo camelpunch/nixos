cad9a15faa46d0a394287315c9f52fb07922d314
Andrew Bruce <me@andrewbruce.net>
2023-03-18 08:17:02 +0000

    Suppress output from fly existence check

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -159,7 +159,7 @@
     bashrcExtra = ''
       export PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
       source <(k3s completion bash)
-      if command -v fly 2>/dev/null
+      if command -v fly > /dev/null
       then
         source <(fly completion --shell=bash)
       fi
