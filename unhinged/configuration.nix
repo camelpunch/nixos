35d2f7f932f497a89a20ea19c9b3ee396b7dc3b1
Andrew Bruce <me@andrewbruce.net>
2023-04-04 16:03:56 +0100

    Partially revert "Rename affable to hosting.code.supply"
    This reverts commit 35ee2b8b96a063ca286a181784e6556ef18ecdb6.
    
    Bring back db config for now

--- unhinged/configuration.nix
+++ unhinged/configuration.nix
@@ -162,14 +162,14 @@ in
     enable = true;
     enableTCPIP = true;
     ensureDatabases = [
-      "hosting"
+      "affable"
       "atc"
     ];
     ensureUsers = [
       {
-        name = "hosting";
+        name = "affable";
         ensurePermissions = {
-          "DATABASE hosting" = "ALL PRIVILEGES";
+          "DATABASE affable" = "ALL PRIVILEGES";
         };
       }
       {
@@ -184,8 +184,8 @@ in
       local   all       all                         trust
       host    all       all       127.0.0.1/32      trust
       host    all       all       ::1/128           trust
-      host    hosting   hosting   192.168.1.0/24    trust
-      host    hosting   hosting   10.42.0.0/16      trust
+      host    affable   affable   192.168.1.0/24    trust
+      host    affable   affable   10.42.0.0/16      trust
       host    atc       concourse 192.168.1.0/24    trust
       host    atc       concourse 10.42.0.0/16      trust
     '';
