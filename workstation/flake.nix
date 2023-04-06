8061b843c3cb7cd5bdef98604bd9eea28c456373
Andrew Bruce <me@andrewbruce.net>
2023-04-06 11:54:47 +0100

    Use latest nixpkgs for workstation

--- workstation/flake.nix
+++ workstation/flake.nix
@@ -1,4 +1,8 @@
 {
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+  };
+
   outputs = { self, nixpkgs }: {
     nixosConfigurations.p14s = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
