8f30cc0ebb7d9ec0e8d54812e4812682999f663e
Andrew Bruce <me@andrewbruce.net>
2023-04-06 12:04:11 +0100

    Reinstate alsa-scarlett-gui
    Now that unstable is tracked, we can avoid the special case. Next step
    is to get home-manager to use the same flake.

--- workstation/configuration.nix
+++ workstation/configuration.nix
@@ -4,9 +4,6 @@
 
 { config, lib, pkgs, ... }:
 
-# let
-#   unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
-# in
 {
   nix = {
     package = pkgs.nixUnstable;
@@ -150,7 +147,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     k3s
-    # unstable.alsa-scarlett-gui
+    alsa-scarlett-gui
   ];
 
   programs.ssh.startAgent = false;
