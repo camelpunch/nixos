6bfa08b7db8a7ec7df027d528de0c125ffb9332a
Andrew Bruce <me@andrewbruce.net>
2023-06-18 11:53:26 +0100

    Set low latency pipewire config

--- workstation/configuration.nix
+++ workstation/configuration.nix
@@ -126,6 +126,21 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+  environment.etc =
+    let
+      json = pkgs.formats.json { };
+    in
+    {
+      "pipewire/pipewire.d/92-low-latency.conf".source = json.generate "92-low-latency.conf" {
+        context.properties = {
+          default.clock.rate = 48000;
+          default.clock.quantum = 128;
+          default.clock.min-quantum = 128;
+          default.clock.max-quantum = 128;
+        };
+      };
+    };
+
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
