22c85af5137f2f74bd5808ae24df0a7c9eed55de
Andrew Bruce <me@andrewbruce.net>
2023-07-18 21:00:53 +0100

    Enable repl-flake

--- workstation/configuration.nix
+++ workstation/configuration.nix
@@ -9,7 +9,7 @@
   nix = {
     package = pkgs.nixUnstable;
     extraOptions = ''
-      experimental-features = nix-command flakes
+      experimental-features = nix-command flakes repl-flake
       keep-derivations = true
       keep-outputs = true
     '';
