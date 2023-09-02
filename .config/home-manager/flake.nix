243bcf1d71768ce3525babaea2e2084daa8c30b0
Andrew Bruce <me@andrewbruce.net>
2023-09-02 16:54:11 +0100

    Bump nixpkgs in home-manager

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of andrew";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
