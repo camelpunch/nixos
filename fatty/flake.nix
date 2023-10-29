d9492d015b3cd102f80d0c87b4cfa117b55e9f64
Andrew Bruce <me@andrewbruce.net>
2023-10-29 13:14:38 +0000

    fatty's flake

---
+++ fatty/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.fatty = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
+}
