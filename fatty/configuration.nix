2d51aeeb43f94a9452a3ef35f45966bc531e316b
Andrew Bruce <me@andrewbruce.net>
2023-10-29 13:08:49 +0000

    Don't start ssh agent on fatty

--- fatty/configuration.nix
+++ fatty/configuration.nix
@@ -117,6 +117,8 @@
     #  wget
   ];
 
+  programs.ssh.startAgent = false;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
