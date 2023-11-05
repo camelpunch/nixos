e1aa1fd67eb0ae065f1193fab9366b2baa917444
Andrew Bruce <me@andrewbruce.net>
2023-11-05 14:57:38 +0000

    format flake.nix

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -13,31 +13,33 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    git-mob,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    formatter.${system} = pkgs.nixpkgs-fmt;
-    homeConfigurations.andrew = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
+  outputs =
+    { nixpkgs
+    , home-manager
+    , git-mob
+    , ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+      homeConfigurations.andrew = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-      # Specify your home configuration modules here, for example,
-      # the path to your home.nix.
-      modules = [./home.nix ./neovim.nix ./git.nix];
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ./neovim.nix ./git.nix ];
 
-      # Optionally use extraSpecialArgs
-      # to pass through arguments to home.nix
-      extraSpecialArgs = {
-        inherit git-mob;
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        extraSpecialArgs = {
+          inherit git-mob;
 
-        git.userName = "Andrew Bruce";
-        git.userEmail = "me@andrewbruce.net";
+          git.userName = "Andrew Bruce";
+          git.userEmail = "me@andrewbruce.net";
+        };
       };
     };
-  };
 }
