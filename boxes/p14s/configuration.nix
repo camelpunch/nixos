1460c002265d3c6f6dd600db16631a2c568e8608
Andrew Bruce <me@andrewbruce.net>
2024-01-02 13:48:09 +0000

    Move p14s network stuff to own file

--- boxes/p14s/configuration.nix
+++ boxes/p14s/configuration.nix
@@ -9,19 +9,8 @@
     ./nix.nix
     ./hardware-configuration.nix
     ./boot.nix
+    ./network.nix
   ];
-
-  networking.hostName = "p14s"; # Define your hostname.
-  networking.extraHosts = ''
-    127.0.0.1 hosting.code.test
-  '';
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/London";
 
@@ -131,33 +120,5 @@
   programs.ssh.startAgent = false;
   services.pcscd.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-  networking.firewall.allowedTCPPorts = [
-    6443
-  ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 }
