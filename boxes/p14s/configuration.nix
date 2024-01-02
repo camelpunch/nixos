bf434f728e7310b60c2fd141a64de8035b81d83b
Andrew Bruce <me@andrewbruce.net>
2024-01-02 13:44:04 +0000

    Move p14s boot config to own file

--- boxes/p14s/configuration.nix
+++ boxes/p14s/configuration.nix
@@ -8,20 +8,8 @@
   imports = [
     ./nix.nix
     ./hardware-configuration.nix
+    ./boot.nix
   ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
-  # boot.kernelPackages = pkgs.linuxPackages_6_1;
-
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
-
-  boot.initrd.luks.devices."luks-26861216-bdc0-4196-a13f-1d9b8caaebfe".device = "/dev/disk/by-uuid/26861216-bdc0-4196-a13f-1d9b8caaebfe";
-  boot.initrd.luks.devices."luks-26861216-bdc0-4196-a13f-1d9b8caaebfe".keyFile = "/crypto_keyfile.bin";
 
   networking.hostName = "p14s"; # Define your hostname.
   networking.extraHosts = ''
