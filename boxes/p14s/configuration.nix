5c0a5fe67c354781195f357dd60310a4921ea9f3
Andrew Bruce <me@andrewbruce.net>
2024-01-02 13:38:51 +0000

    Move p14s' nix config into own file

--- boxes/p14s/configuration.nix
+++ boxes/p14s/configuration.nix
@@ -5,21 +5,8 @@
 , pkgs
 , ...
 }: {
-  nix = {
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes repl-flake auto-allocate-uids configurable-impure-env
-      keep-derivations = true
-      keep-outputs = true
-    '';
-    settings = {
-      trusted-users = [ "andrew" ];
-      substituters = [ "https://nixos-homepage.cachix.org" ];
-      trusted-public-keys = [ "nixos-homepage.cachix.org-1:NHKBt7NjLcWfgkX4OR72q7LVldKJe/JOsfIWFDAn/tE=" ];
-    };
-  };
-
   imports = [
+    ./nix.nix
     ./hardware-configuration.nix
   ];
 
