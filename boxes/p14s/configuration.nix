fdb7029b4547c9ca4cae313c31577c944cea35aa
Andrew Bruce <me@andrewbruce.net>
2024-01-02 14:17:39 +0000

    Separate user and gpg ssh stuff for p14s

--- boxes/p14s/configuration.nix
+++ boxes/p14s/configuration.nix
@@ -1,45 +1,20 @@
-{ pkgs
-, ...
-}: {
+{
   imports = [
-    ./nix.nix
-    ./hardware-configuration.nix
     ./boot.nix
-    ./network.nix
-    ./locale.nix
+    ./gpg-ssh.nix
     ./gui.nix
+    ./hardware-configuration.nix
+    ./locale.nix
+    ./network.nix
+    ./nix.nix
     ./sound.nix
+    ./user.nix
   ];
 
-  virtualisation.docker.enable = true;
-  services.printing.enable = true;
   services.fwupd.enable = true;
+  services.printing.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.andrew = {
-    isNormalUser = true;
-    description = "Andrew Bruce";
-    extraGroups = [
-      "audio"
-      "docker"
-      "networkmanager"
-      "wheel"
-    ];
-  };
-
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "andrew";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
-  programs.ssh.startAgent = false;
-  services.pcscd.enable = true;
+  virtualisation.docker.enable = true;
 
   system.stateVersion = "22.11"; # Did you read the comment?
 }
