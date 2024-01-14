e77889701227cae6589fac499947e12bdd9daa43
Andrew Bruce <me@andrewbruce.net>
2024-01-14 16:42:41 +0000

    Use latest dnsmasq config schema

--- boxes/unhinged/dnsmasq.nix
+++ boxes/unhinged/dnsmasq.nix
@@ -11,21 +11,23 @@ in
   services.resolved.enable = false;
   services.dnsmasq = {
     enable = true;
-    servers = [
-      "8.8.8.8"
-      "8.8.4.4"
-      "2001:4860:4860::8888"
-      "2001:4860:4860::8844"
-    ];
-    extraConfig = ''
-      no-hosts
-      no-resolv
-      no-poll
-      address=/*.code.test/127.0.0.1
-      address=/*.code.supply/${ipv4}
-      address=/*.code.supply/${ipv6}
-      address=/unhinged/${ipv4}
-      address=/unhinged/${ipv6}
-    '';
+    settings = {
+      no-hosts = true;
+      no-resolv = true;
+      no-poll = true;
+      address = [
+        "/*.code.test/127.0.0.1"
+        "/*.code.supply/${ipv4}"
+        "/*.code.supply/${ipv6}"
+        "/unhinged/${ipv4}"
+        "/unhinged/${ipv6}"
+      ];
+      server = [
+        "8.8.8.8"
+        "8.8.4.4"
+        "2001:4860:4860::8888"
+        "2001:4860:4860::8844"
+      ];
+    };
   };
 }
