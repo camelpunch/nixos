5942fcce4d788164769b644e0d0b4f5593442d5a
Andrew Bruce <me@andrewbruce.net>
2024-02-04 20:14:13 +0000

    Inline firefox settings

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -4,21 +4,15 @@
   programs.firefox = {
     enable = true;
     profiles =
-      let
-        baseSettings = {
-          "apz.gtk.pangesture.delta_mode" = 2;
-          "signon.rememberSignons" = false;
-        };
-        baseSearch = {
-          force = true;
-        };
-      in
       {
         "Andrew" = {
           isDefault = true;
           id = 0;
-          settings = baseSettings;
-          search = baseSearch;
+          settings = {
+            "apz.gtk.pangesture.delta_mode" = 2;
+            "signon.rememberSignons" = false;
+          };
+          search.force = true;
         };
       };
   };
